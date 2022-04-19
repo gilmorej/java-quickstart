711db4dc0fc8bd625eb053ce23e2a9bd3ef5eced
James Gilmore <gilmorej@gmail.com>
2022-04-19 15:31:40 -0600

    Base spring java project

---
+++ src/main/java/com/gilmore/javaproject/JavaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gilmore.javaproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaProjectApplication.class, args);
+	}
+
+}
